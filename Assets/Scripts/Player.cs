d5ace2bf90c17245f4c214cbd346d68b05262e5a
DarkTiger <darktigerdevelop@gmail.com>
2018-02-06 17:37:34 +0100

    First implementation of shoot and weapons

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Texture2D aimTexture = null;
+    LineRenderer lineRenderer = null;
+    public GameObject currentWeapon = null; 
+        
+
+    void Start ()
+    {
+        Cursor.SetCursor(aimTexture, new Vector2(0, 0), CursorMode.Auto);
+        lineRenderer = GetComponent<LineRenderer>();
+
+        GameObject weaponPrefab = Resources.Load("Weapons/Winchester") as GameObject;
+        currentWeapon = Instantiate(weaponPrefab, transform.position, Quaternion.identity);
+    }
+	
+	
+	void Update ()
+    {
+        Vector3 pos = transform.position;
+        pos.x += 0.5f;
+        pos.y += 0.4f;
+        currentWeapon.transform.position = pos;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentWeapon.transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - currentWeapon.transform.position);
+
+        if (currentWeapon != null)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                currentWeapon.GetComponent<Weapon>().Fire();
+            }
+        }
+    }   
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public enum WeaponType { OneShot, Spread };
+    public WeaponType weaponType;
+    public GameObject bullet;
+    public float damage = 1;
+    public float firePower = 1;
+    public float fireRate = 1;
+    public float spreadWidth = 5;
+    public int spreadBulletCount = 8;
+    float heatLevel = 0;
+    Transform bulletSpawn;
+    float lastFireTime = 0;
+    
+    //
+    void Start()
+    {
+        bulletSpawn = transform.GetChild(0);
+    }
+
+
+    void Update()
+    {
+        Debug.Log(Input.mousePosition + "   " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+    
+
+    public void Fire()
+    {
+        if (Time.time >= lastFireTime + fireRate)
+        {
+            if (weaponType == WeaponType.OneShot)
+            {
+                GameObject newBullet = Instantiate(bullet, bulletSpawn.position, Quaternion.identity);
+                newBullet.GetComponent<Rigidbody2D>().AddForce(transform.up * firePower);
+                Destroy(newBullet, 2);
+            }
+            else if (weaponType == WeaponType.Spread)
+            {
+                Vector3[] directions = new Vector3[spreadBulletCount];
+                for (int i = 0; i < spreadBulletCount; i++)
+                {
+                    Vector3 mousePosTemp = Input.mousePosition;
+                    //mousePosTemp.y += 
+
+                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                    float bulletOffset = Random.Range(-spreadWidth, spreadWidth);
+                    mousePos.y *= bulletOffset;
+                    mousePos.x /= bulletOffset;
+
+                    Vector3 direction = mousePos - transform.position;
+                    GameObject newBullet = Instantiate(bullet, bulletSpawn.position, Quaternion.identity);
+                    newBullet.GetComponent<Rigidbody2D>().AddForce(direction * firePower);
+                    Destroy(newBullet, 2);
+                }               
+            }
+           
+            lastFireTime = Time.time;
+        }
+    }
+}
