55bd9921186d8ca32af3ee9bbc6888462f90b6c0
Andrea Franciosi <french_04@hotmail.it>
2018-02-06 17:22:58 +0100

    shop

--- Assets/Scripts/EnablerWeapon.cs
+++ Assets/Scripts/EnablerWeapon.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI;
 
 public class EnablerWeapon : MonoBehaviour
 {
-    bool imEnabled = true;
+    [HideInInspector]
+    public bool imEnabled = true;
+
+    [HideInInspector]
+    public bool purchased = false;
+
     Image myImage;
-    public int myValue = 0;
+
+    public int organ1value = 0, organ2value = 0, organ3value = 0, organ4value = 0, organ5value = 0, organ6value = 0;
 
     Shop shop;
 
@@ -21,23 +27,45 @@ public class EnablerWeapon : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		if(shop.money < myValue && imEnabled)
+        //if im not purchasede
+        if (!purchased)
         {
-            Debug.Log("disabled");
-            imEnabled = false;
-            Color myColor;
-            myColor = Color.white;
-            myColor.a = 0.2f;
-            myImage.color = myColor;
+            //if i have money
+            if(shop.organ1 >= organ1value && shop.organ2 >= organ2value && shop.organ3 >= organ3value && shop.organ4 >= organ4value && shop.organ5 >= organ5value && shop.organ6 >= organ6value)
+            {
+                //if i am disabled
+                if (!imEnabled)
+                {
+                    imEnabled = true;
+                    Color myColor;
+                    myColor = Color.white;
+                    myColor.a = 1;
+                    myImage.color = myColor;
+                }
+            }
+            else
+            {
+                //if i am enabled
+                if (imEnabled)
+                {
+                    imEnabled = false;
+                    Color myColor;
+                    myColor = Color.white;
+                    myColor.a = 0.2f;
+                    myImage.color = myColor;
+                }
+            }
         }
-        else if(shop.money >= myValue && !imEnabled)
-        {
-            Debug.Log("enabled");
-            imEnabled = true;
-            Color myColor;
-            myColor = Color.white;
-            myColor.a = 1;
-            myImage.color = myColor;
+        else //if i have already buy this weapon
+        {            
+            if (!imEnabled)
+            {
+                imEnabled = true;
+                Color myColor;
+                myColor = Color.white;
+                myColor.a = 1;
+                myImage.color = myColor;
+            }
         }
 	}
 }
--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -5,16 +5,41 @@ using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
-    public Image[] Weapon;
+    public EnablerWeapon[] Weapon;
 
-    [HideInInspector]
-    public int money = 0;
+    //[HideInInspector]
+    public int organ1 = 0, organ2 = 0, organ3 = 0, organ4 = 0, organ5 = 0, organ6 = 0;
 
     [HideInInspector]
     public int WeaponSelected = 0;
 
+    private void Start()
+    {
+        Weapon[0].purchased = true;
+    }
+
     public void SelectWeapon(int choise)
     {
-        WeaponSelected = choise;
+        //if the weapon is buyable
+        if (Weapon[choise].imEnabled)
+        {
+            //if i have already buy this weapon
+            if(Weapon[choise].purchased == true)
+            {
+                WeaponSelected = choise;
+            }
+            else
+            {
+                organ1 -= Weapon[choise].organ1value;
+                organ2 -= Weapon[choise].organ2value;
+                organ3 -= Weapon[choise].organ3value;
+                organ4 -= Weapon[choise].organ4value;
+                organ5 -= Weapon[choise].organ5value;
+                organ6 -= Weapon[choise].organ6value;
+
+                WeaponSelected = choise;
+                Weapon[choise].purchased = true;
+            }
+        }
     }
 }
