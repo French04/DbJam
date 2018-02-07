552245ac8bd3fc7b5bad7feac3b93f650fd827dc
Andrea Franciosi <french_04@hotmail.it>
2018-02-07 14:06:56 +0100

    aggiunto singleton sull'hud

--- Assets/Scripts/OrganCollector.cs
+++ Assets/Scripts/OrganCollector.cs
@@ -7,13 +7,24 @@ public class OrganCollector : MonoBehaviour
     [HideInInspector]
     public int heart = 0, brain = 0, lung = 0, kidney = 0, intestine = 0, liver = 0;
 
-    //Shop shop;
+    public static OrganCollector instance;
 
     // Use this for initialization
     void Start ()
     {
-        //shop = FindObjectOfType<Shop>();
-	}
+        //Check if instance already exists
+        if (instance == null)
+        {
+            //if not, set instance to this
+            instance = this;
+        }
+        //If instance already exists and it's not this:
+        else if (instance != this)
+        {
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
