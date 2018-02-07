9067d377f571253ea686359d587b47ae6a0786f3
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2018-02-07 21:31:12 +0100

    main menu fwuhguew

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,8 +14,20 @@ public class GameManager : MonoBehaviour
 
     public static void NextLevel()
     {
-        LevelCounter.currentLevel++;
-        SceneManager.LoadScene(LevelCounter.currentLevel);
+        if (LevelCounter.currentLevel < 13)
+        {
+            LevelCounter.currentLevel++;
+            SceneManager.LoadScene(LevelCounter.currentLevel);
+        }
+        else
+        {
+            print("Game Over");
+        }
+    }
+
+    public static void JumpToLevel(int i)
+    {
+        SceneManager.LoadScene(i);
     }
 
 }
