a7b33f35d99bd2e839ba9fcbee44110f1c2c1fa6
dbga_34 <dbga_34@dbga.local>
2018-02-07 23:35:16 +0100

    blooooooood

---
+++ Assets/DestroyBlood.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBlood : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        
+        StartCoroutine(DestroyBloods());
+    }
+
+
+    IEnumerator DestroyBloods()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(gameObject);
+    }
+}
